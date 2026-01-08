88d0aa5b6ca71496666458d50dc4505e2a776c7a
Guilherme Pires Figueiredo <guilherme78965@gmail.com>
2026-01-08 16:11:56 +0000

    added tags for alloy ingredients

---
+++ src/main/java/com/figueiredo/everdalemod/util/ModTags.java
@@ -0,0 +1,29 @@
+package com.figueiredo.everdalemod.util;
+
+import com.figueiredo.everdalemod.EverdaleMod;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+    public static class Blocks {
+        public static final TagKey<Block> ALLOY_INGREDIENT_BLOCK = tag("alloy_ingredient_block");
+
+
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(EverdaleMod.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+        public static final TagKey<Item> ALLOY_INGREDIENT_ITEM = tag("alloy_ingredient_item");
+
+
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(EverdaleMod.MOD_ID, name));
+        }
+    }
+}
